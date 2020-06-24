0bc9c30086ab12d9732ffbe01b9931f12afadaf7
ho823 <amelyne.ho@hotmail.fr>
2020-06-24 17:36:01 +0200

    footer is done

--- doctolib/src/components/Dashboard.jsx
+++ doctolib/src/components/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 
 function Dashboard() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
