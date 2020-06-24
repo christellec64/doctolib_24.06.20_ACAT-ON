a638ef5d834dba49c7edc7ad89f0129f693f9ad7
ho823 <amelyne.ho@hotmail.fr>
2020-06-24 16:19:29 +0200

    Added Router and component dashboard

--- doctolib/src/App.js
+++ doctolib/src/App.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Home from './components/Home';
 
 function App() {
   return (
+    <>
+    <Router>
+      <div>
+        <Link to ='/'></Link>
+        <Link to ='/signin'></Link>
+        <Link to ='/createaccount'></Link>
+        <Link to ='/information'></Link>
+        <Link to ='/treatment   '></Link>
+        <Link to ='/pilllist'></Link>
+        <Link to ='/stat'></Link>
+        <Link to ='/ordonance '></Link>
+        <Link to ='/dashboard'></Link>
+      </div>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/signin' />
+        <Route exact path='/createaccount' />
+        <Route exact path='/information' />
+        <Route exact path='/treatment' />
+        <Route exact path='/pilllist' />
+        <Route exact path='/stat' />
+        <Route exact path='/ordonance' />
+        <Route exact path='/dashboard' />
+      </Switch>
+    </Router>
+    
     <div className="App">
       <Home />
     </div>
+    </>
   );
 }
 
---
+++ doctolib/src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Dashboard.module.css'
+
+function Dashboard() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Dashboard;
