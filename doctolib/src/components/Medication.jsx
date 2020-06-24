fd0c621689710139c573a586b2df860a9cb7724f
Christellec64 <christelle.cwcs64@gmail.com>
2020-06-24 23:11:21 +0200

    creating medication component

--- doctolib/src/components/Medication.jsx
+++ doctolib/src/components/Medication.jsx
@@ -1,62 +1,191 @@
-import React from "react";
-import { Container, Table } from "reactstrap";
+import React, { useState } from "react";
+import { Container, Table, Row, Col, Button, Collapse } from "reactstrap";
 
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import styles from "./Medication.module.css";
-// import alarm from "../img/clock.png"
+import alarm from "../img/alarm.png";
 
-function Medication() {
+function Medication () {
+  const [collapseA, setCollapseA] = useState(false);
+  const [collapseB, setCollapseB] = useState(false);
+  const [collapseC, setCollapseC] = useState(false);
+  const [collapseD, setCollapseD] = useState(false);
+
+  const toggleA = () => setCollapseA(!collapseA);
+  const toggleB = () => setCollapseB(!collapseB);
+  const toggleC = () => setCollapseC(!collapseC);
+  const toggleD = () => setCollapseD(!collapseD);
+
+  const [status, setStatus] = useState("Closed");
+
+  const onEntering = () => setStatus("Opening...");
+
+  const onEntered = () => setStatus("Opened");
+
+  const onExiting = () => setStatus("Closing...");
+
+  const onExited = () => setStatus("Closed");
   return (
     <>
-      <Navbar />
-      <Container>
-          <div className={styles.nextMeditationAlert}>
-              {/* <img src={alarm} alt="alarm logo"/> */}
-              <h1>Next medication in 73 minutes</h1>
-              <h2>Pillule D</h2>
-          </div>
-        <Table>
+      <Navbar title="My medication" />
+      <Container className={styles.nextMeditationContainer}>
+        <Row className={styles.nextMeditationAlert}>
+          <Col xs="2">
+            <img
+              src={alarm}
+              alt="alarm logo"
+              width="50vw"
+              className="xs-auto"
+            />
+          </Col>
+          <Col>
+            <p>Next medication in 73 minutes</p>
+            <p>Pillule D</p>
+          </Col>
+        </Row>
+      </Container>
+      <Container className={styles.medicationTableContainer}>
+        <Table className={styles.medicationTable}>
           <thead>
             <tr>
-              <th>
-                <input type="checkbox" />
-              </th>
+              <th />
               <th>Name</th>
-              <th>Frequency</th>
+              <th className={styles.colRight}>Frequency</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <th scope="row">
-                <input type="checkbox" />
+                <input type="checkbox" onClick={toggleA} />
               </th>
               <td>Pillule A</td>
-              <td>3 per day</td>
+              <td className={styles.colRight}>3 per day</td>
+            </tr>
+            <tr>
+              <td />
+              <td />
+              <td className={styles.colRight}>
+                <Collapse
+                  isOpen={collapseA}
+                  onEntering={onEntering}
+                  onEntered={onEntered}
+                  onExiting={onExiting}
+                  onExited={onExited}
+                >
+                  <Button className={styles.buttonMedication}>Edit</Button>
+                </Collapse>
+                <Collapse
+                  isOpen={collapseA}
+                  onEntering={onEntering}
+                  onEntered={onEntered}
+                  onExiting={onExiting}
+                  onExited={onExited}
+                >
+                  <Button className={styles.buttonMedication}>Delete</Button>
+                </Collapse>
+              </td>
             </tr>
             <tr>
               <th scope="row">
-                <input type="checkbox" />
+                <input type="checkbox" onClick={toggleB} />
               </th>
               <td>Pillule B</td>
-              <td>2 per week</td>
+              <td className={styles.colRight}>2 per week</td>
+            </tr>
+            <tr>
+              <td />
+              <td />
+              <td className={styles.colRight}>
+                <Collapse
+                  isOpen={collapseB}
+                  onEntering={onEntering}
+                  onEntered={onEntered}
+                  onExiting={onExiting}
+                  onExited={onExited}
+                >
+                  <Button className={styles.buttonMedication}>Edit</Button>
+                </Collapse>
+                <Collapse
+                  isOpen={collapseB}
+                  onEntering={onEntering}
+                  onEntered={onEntered}
+                  onExiting={onExiting}
+                  onExited={onExited}
+                >
+                  <Button className={styles.buttonMedication}>Delete</Button>
+                </Collapse>
+              </td>
             </tr>
             <tr>
               <th scope="row">
-                <input type="checkbox" />
+                <input type="checkbox" onClick={toggleC} />
               </th>
               <td>Pillule C</td>
-              <td>1 per day</td>
+              <td className={styles.colRight}>1 per day</td>
+            </tr>
+            <tr>
+              <td />
+              <td />
+              <td className={styles.colRight}>
+                <Collapse
+                  isOpen={collapseC}
+                  onEntering={onEntering}
+                  onEntered={onEntered}
+                  onExiting={onExiting}
+                  onExited={onExited}
+                >
+                  <Button className={styles.buttonMedication}>Edit</Button>
+                </Collapse>
+                <Collapse
+                  isOpen={collapseC}
+                  onEntering={onEntering}
+                  onEntered={onEntered}
+                  onExiting={onExiting}
+                  onExited={onExited}
+                >
+                  <Button className={styles.buttonMedication}>Delete</Button>
+                </Collapse>
+              </td>
             </tr>
             <tr>
               <th scope="row">
-                <input type="checkbox" />
+                <input type="checkbox" onClick={toggleD} />
               </th>
               <td>Pillule D</td>
-              <td>3 per day</td>
+              <td className={styles.colRight}>3 per day</td>
+            </tr>
+            <tr>
+              <td />
+              <td />
+              <td className={styles.colRight}>
+                <Collapse
+                  isOpen={collapseD}
+                  onEntering={onEntering}
+                  onEntered={onEntered}
+                  onExiting={onExiting}
+                  onExited={onExited}
+                >
+                  <Button className={styles.buttonMedication}>Edit</Button>
+                </Collapse>
+                <Collapse
+                  isOpen={collapseD}
+                  onEntering={onEntering}
+                  onEntered={onEntered}
+                  onExiting={onExiting}
+                  onExited={onExited}
+                >
+                  <Button className={styles.buttonMedication}>Delete</Button>
+                </Collapse>
+              </td>
             </tr>
           </tbody>
         </Table>
+        <Row>
+          <Col xs={{ size: 6, offset: 8 }}>
+            <Button className={styles.buttonMedication}>Add medication</Button>
+          </Col>
+        </Row>
       </Container>
       <Footer />
     </>
--- doctolib/src/components/Navbar.jsx
+++ doctolib/src/components/Navbar.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Navbar.module.css'
-import logo from '../img/logo.png';
-import bell from '../img/bell.png';
-import user from '../img/user.png';
-import logout from '../img/logout.png';
+import styles from "./Navbar.module.css";
+import logo from "../img/logo.png";
+import bell from "../img/bell.png";
+import user from "../img/user.png";
+import logout from "../img/logout.png";
 
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col } from "reactstrap";
 
-function Navbar() {
+function Navbar({ title }) {
   return (
     <>
       <Container className={styles.navbar}>
         <Row className={styles.listItem}>
-          <Col xs='6'><img src={logo} alt='logo app' className={styles.logo} />MediMe</Col>
-          <Col xs='2'><img src={bell} alt='notification' className={styles.item} /></Col>
-          <Col xs='2'><img src={user} alt='user' className={styles.item} /></Col>
-          <Col xs='2'><img src={logout} alt='log out' className={styles.item} /></Col>
+          <Col xs="2">
+            <Row>
+              <Col>
+                <img src={logo} alt="logo app" className={styles.logo} />
+              </Col>
+            </Row>
+            <Row>
+              <Col className={styles.medime}>MediMe</Col>
+            </Row>
+          </Col>
+          <Col xs={{ size: 5, offset: 0 }} className={styles.title}>{title}</Col>
+          <Col xs="1.5">
+            <img src={bell} alt="notification" className={styles.item} />
+          </Col>
+          <Col xs="2">
+            <img src={user} alt="user" className={styles.item} />
+          </Col>
+          <Col xs="1.5">
+            <img src={logout} alt="log out" className={styles.item} />
+          </Col>
         </Row>
       </Container>
     </>
