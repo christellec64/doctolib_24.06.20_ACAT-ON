5d649d8a5fbb624373bdb8c288e353bc19996e89
Tommmychinn <tommychinn1@gmail.com>
2020-06-24 16:55:05 +0100

    sorting css issues

--- doctolib/src/components/login/Signin.jsx
+++ doctolib/src/components/login/Signin.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Input, Button, Row, Col } from "reactstrap";
+import { Form, Input, Button } from "reactstrap";
 import { Link } from "react-router-dom";
 import styles from "./Signin.module.css";
 
@@ -7,33 +7,31 @@ const SignIn = () => {
     const [email, setEmail] = useState([""]);
     const [password, setPassword] = useState("");
     return (
-        <Row className={styles.background}>
-            <Col sm={{size:6, offset:4}}>
-                <Form className={styles.form}>
-                    <Input
-                        className={styles.inputs}
-                        type="text"
-                        value={email}
-                        placeholder="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <Input
-                        className={styles.inputs}
-                        type="text"
-                        value={password}
-                        placeholder="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
+        <div className={styles.background}>
+            <Form className={styles.form}>
+                <Input
+                    className={styles.inputs}
+                    type="text"
+                    value={email}
+                    placeholder="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                    className={styles.inputs}
+                    type="text"
+                    value={password}
+                    placeholder="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
 
-                    <Link to="/home">
-                        <Button className={styles.button}>Submit</Button>
-                    </Link>
-                </Form>
-            </Col>
-            {/* {email.map(item=>(
-            <p>{item}</p>
-        ))} */}
-        </Row>
+                <Link to="/home">
+                    <Button className={styles.button}>Submit</Button>
+                </Link>
+                <Link to="/createaccount">
+                    <Button className={styles.button2}>Create Account</Button>
+                </Link>
+            </Form>
+        </div>
     );
 };
 
