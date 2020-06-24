4fece2371dc37e06943da4fc36bf79c2f100f808
Christellec64 <christelle.cwcs64@gmail.com>
2020-06-24 19:44:04 +0200

    adding medication component in app.js and create table in medication component

--- doctolib/src/App.js
+++ doctolib/src/App.js
@@ -1,40 +1,37 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Home from './components/Home';
-import Dashboard from './components/Dashboard';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
+import Medication from "./components/Medication";
 
 function App() {
   return (
     <>
-    <Router>
-      <div>
-        <Link to ='/'></Link>
-        <Link to ='/signin'></Link>
-        <Link to ='/createaccount'></Link>
-        <Link to ='/information'></Link>
-        <Link to ='/treatment   '></Link>
-        <Link to ='/pilllist'></Link>
-        <Link to ='/stat'></Link>
-        <Link to ='/ordonance '></Link>
-        <Link to ='/dashboard'></Link>
-      </div>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/signin' />
-        <Route exact path='/createaccount' />
-        <Route exact path='/information' />
-        <Route exact path='/treatment' />
-        <Route exact path='/pilllist' />
-        <Route exact path='/stat' />
-        <Route exact path='/ordonance' />
-        <Route exact path='/dashboard' component={Dashboard} />
-      </Switch>
-    </Router>
+      <Router>
+        <div>
+          <Link to="/"></Link>
+          <Link to="/signin"></Link>
+          <Link to="/createaccount"></Link>
+          <Link to="/information"></Link>
+          <Link to="/treatment   "></Link>
+          <Link to="/pilllist"></Link>
+          <Link to="/stat"></Link>
+          <Link to="/ordonance "></Link>
+          <Link to="/dashboard"></Link>
+        </div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" />
+          <Route exact path="/createaccount" />
+          <Route exact path="/information" />
+          <Route exact path="/treatment" />
+          <Route exact path="/pilllist" />
+          <Route exact path="/stat" />
+          <Route exact path="/ordonance" />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/medication" component={Medication} />
+        </Switch>
+      </Router>
     </>
   );
 }
