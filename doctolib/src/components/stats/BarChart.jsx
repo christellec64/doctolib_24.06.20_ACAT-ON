b555982fc375abd6130db0550f07ab05cce5cefe
nanou-11 <anais.jouaret@gmail.com>
2020-06-25 15:15:50 +0200

    add dynamic values to bar

--- doctolib/src/components/stats/BarChart.jsx
+++ doctolib/src/components/stats/BarChart.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
 
-import styles from "./Stat.module.css";
+import { api_url } from "../../api.js";
 
-const state = {
+import styles from "./Stat.module.css";
+import Axios from "axios";
+
+const data = {
   labels: [
+    "23/06",
+    "8am",
+    "Lunch",
+    "Diner",
+    "24/06",
     "Morning",
     "Noon",
     "Evening",
-    "Morning",
-    "Noon",
-    "Evening",
+    "25/06",
     "Morning",
     "Noon",
     "Evening",
@@ -21,22 +27,135 @@ const state = {
       backgroundColor: " #f3724a",
       borderColor: " #f3724a",
       borderWidth: 2,
-      data: [0.8, 2.1, 1.6, 0.9, 1.2],
+      data: [0, 0.8, 2.1, 1.6, 0, 0.9, 1.2, 1.1, 0, 0.9, 2.0, 1.8],
     },
   ],
 };
 
-export default class BarChart extends React.Component {
+class BarChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      label: "",
+      values: [],
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    this.getGlucose();
+  }
+
+  getGlucose() {
+    Axios.get(`${api_url}/glucose`)
+      .then(({ data }) => this.setState({ values: data }))
+      .catch((err) => this.setState({ error: err }));
+  }
+
   render() {
+    console.log(this.state.values);
+    const { values } = this.state;
     return (
       <>
         <Bar
           className={styles.bar2}
-          data={state}
+          data={{
+            labels: [
+              `${
+                values &&
+                values[values.length - 3] &&
+                values[values.length - 3].date
+              }`,
+              "8am",
+              "Lunch",
+              "Diner",
+              `${
+                values &&
+                values[values.length - 2] &&
+                values[values.length - 2].date
+              }`,
+              "Morning",
+              "Noon",
+              "Evening",
+              `${
+                values &&
+                values[values.length - 1] &&
+                values[values.length - 1].date
+              }`,
+              "Morning",
+              "Noon",
+              "Evening",
+            ],
+            datasets: [
+              {
+                label: "Glucose statement",
+                backgroundColor: " #f3724a",
+                borderColor: " #f3724a",
+                borderWidth: 2,
+                data: [
+                  0,
+                  `${
+                    values &&
+                    values[values.length - 3] &&
+                    values[values.length - 3].breakfast
+                  }`,
+                  `${
+                    values &&
+                    values[values.length - 3] &&
+                    values[values.length - 3].lunch
+                  }`,
+                  `${
+                    values &&
+                    values[values.length - 3] &&
+                    values[values.length - 3].diner
+                  }`,
+                  0,
+                  `${
+                    values &&
+                    values[values.length - 2] &&
+                    values[values.length - 2].breakfast
+                  }`,
+                  `${
+                    values &&
+                    values[values.length - 2] &&
+                    values[values.length - 2].lunch
+                  }`,
+                  `${
+                    values &&
+                    values[values.length - 2] &&
+                    values[values.length - 2].diner
+                  }`,
+                  0,
+                  `${
+                    values &&
+                    values[values.length - 1] &&
+                    values[values.length - 1].breakfast
+                  }`,
+                  `${
+                    values &&
+                    values[values.length - 1] &&
+                    values[values.length - 1].lunch
+                  }`,
+                  `${
+                    values &&
+                    values[values.length - 1] &&
+                    values[values.length - 1].diner
+                  }`,
+                ],
+              },
+            ],
+          }}
           options={{
             legend: {
               display: false,
               position: "right",
+            },
+            scales: {
+              xAxes: [
+                {
+                  barThickness: 10,
+                },
+              ],
             },
           }}
         />
@@ -44,3 +163,4 @@ export default class BarChart extends React.Component {
     );
   }
 }
+export default BarChart;
--- doctolib/src/components/stats/Stat.jsx
+++ doctolib/src/components/stats/Stat.jsx
@@ -12,67 +12,88 @@ function stat() {
   const jour = new Date().getDate();
   const month = new Date().getMonth();
   const year = new Date().getFullYear();
+  const monthLetter = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   return (
     <>
       <Navbar title="My stats" />
       <h5 className={`${styles.h5} mt-2`}>Blood glucose statement</h5>
       <h6 xs="4" className={styles.h6}>
-        Date {jour}/{month + 1}/{year}
+        {" "}
+        Your{" "}
+        {month > 9
+          ? `${jour}/${month + 1}/${year}`
+          : `Date ${jour}/0${month + 1}/${year}`}{" "}
+        statement
       </h6>
-      <Row className={`${styles.rowInput} ml-2 mb-3 mt-3 mr-1`}>
+      <Row className={`${styles.rowInput} ml-2 mb-3 mr-1`}>
         <Col xs="3" className="pl-1 pr-1">
           <FormGroup>
-            <Label for="morning">
+            <Label for="breakfast">
               <Input
                 className={styles.input}
                 type="text"
-                name="morning"
-                id="morning"
-                placeholder="Morning..."
-              />
-            </Label>
-          </FormGroup>
-        </Col>
-        <Col xs="2" className="pl-1 pr-1">
-          <FormGroup>
-            <Label for="Noon">
-              <Input
-                className={styles.input}
-                type="text"
-                name="Noon"
-                id="Noon"
-                placeholder="Noon..."
+                name="breakfast"
+                id="breakfast"
+                placeholder="Breakfast"
               />
             </Label>
           </FormGroup>
         </Col>
         <Col xs="3" className="pl-1 pr-1">
           <FormGroup>
-            <Label for="Evening">
+            <Label for="Lunch">
               <Input
                 className={styles.input}
                 type="text"
-                name="Evening"
-                id="Evening"
-                placeholder="Evening..."
+                name="Lunch"
+                id="Lunch"
+                placeholder="Lunch"
               />
             </Label>
           </FormGroup>
         </Col>
-        <Col xs="3">
+        <Col xs="3" className="pl-1 pr-1">
+          <FormGroup>
+            <Label for="Diner">
+              <Input
+                className={styles.input}
+                type="text"
+                name="Diner"
+                id="Diner"
+                placeholder="Diner"
+              />
+            </Label>
+          </FormGroup>
+        </Col>
+        <Col xs="2">
           <Button className={styles.button}>Save</Button>
         </Col>
       </Row>
       <BarChart />
       <h5 className={styles.h5}>Weight</h5>
-      <Row className={`${styles.rowInput} ml-1 mb-3 mt-3 mr-1`}>
-        <Col className={styles.h6} xs="4">Once a month</Col>
-        <Col xs="5">
+      <Row className={`${styles.rowInput} ml-1 mb-3  mr-1`}>
+        <Col className={styles.h6bis} xs="5">
+          Your weight in {monthLetter[month]}
+        </Col>
+        <Col xs="4">
           <FormGroup className="ml-2">
             <Label for="weight">
               <Input
                 className={styles.input}
-                type="number"
+                type="text"
                 name="weight"
                 id="weight"
                 placeholder="KG"
@@ -86,7 +107,7 @@ function stat() {
       </Row>
       <LineGraph />
 
-      <Footer />
+      <Footer className={styles.footer} />
     </>
   );
 }
