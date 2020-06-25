ce9975b0cbc17f34f822c46468fde47ef5a37899
nanou-11 <anais.jouaret@gmail.com>
2020-06-25 15:25:57 +0200

    add dynamic values to line graph

--- doctolib/src/components/stats/BarChart.jsx
+++ doctolib/src/components/stats/BarChart.jsx
@@ -6,37 +6,10 @@ import { api_url } from "../../api.js";
 import styles from "./Stat.module.css";
 import Axios from "axios";
 
-const data = {
-  labels: [
-    "23/06",
-    "8am",
-    "Lunch",
-    "Diner",
-    "24/06",
-    "Morning",
-    "Noon",
-    "Evening",
-    "25/06",
-    "Morning",
-    "Noon",
-    "Evening",
-  ],
-  datasets: [
-    {
-      label: "Glucose statement",
-      backgroundColor: " #f3724a",
-      borderColor: " #f3724a",
-      borderWidth: 2,
-      data: [0, 0.8, 2.1, 1.6, 0, 0.9, 1.2, 1.1, 0, 0.9, 2.0, 1.8],
-    },
-  ],
-};
-
 class BarChart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      label: "",
       values: [],
       error: null,
     };
