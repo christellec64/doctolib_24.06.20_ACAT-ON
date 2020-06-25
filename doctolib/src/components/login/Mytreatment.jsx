77c772f4cb024fde2bf8a9b877397104fd82ae54
Tommmychinn <tommychinn1@gmail.com>
2020-06-25 12:22:44 +0100

    pages finished

--- doctolib/src/components/login/Mytreatment.jsx
+++ doctolib/src/components/login/Mytreatment.jsx
@@ -8,19 +8,24 @@ const MyTreatment = () => {
     const [dosage, setDosage] = useState([""]);
     const [frequency, setFrequency] = useState([""]);
     const [time, setTime] = useState([""]);
-  
+
+    const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
     const logo = require("../img/logowhite.png");
     return (
         <div className={styles.background}>
             <Container className={styles.navbar}>
                 <Row>
-                    <Col xs="4" className={styles.listItem}>
+                    <Col xs="2" className={styles.listItem}>
                         <img
                             src={logo}
                             alt="logo app"
                             className={styles.logo}
                         />
                         MediMe
+                    </Col>
+                    <Col xs="8" className={styles.heading}>
+                        <h2>My Treatment</h2>
                     </Col>
                 </Row>
                 <Row className={styles.formContainer}>
@@ -46,18 +51,34 @@ const MyTreatment = () => {
                             placeholder="Frequency per day"
                             onChange={(e) => setFrequency(e.target.value)}
                         />
-                        <label className={styles.timelabel}>What time do you take your first medication?
-                        <input
-                            className={styles.inputs}
-                            type="time"
-                            value={time}
-                            placeholder="What time do you take your first medication?"
-                            onChange={(e) => setTime(e.target.value)}
-                        />
+                        <select
+                            className={styles.inputfrequency}
+                            type="text"
+                            value={frequency}
+                            placeholder="Frequency per day"
+                            onChange={(e) => setFrequency(e.target.value)}
+                        >
+                            {numbers.map((number) => (
+                                <option value={number}>{number}</option>
+                            ))}
+                        </select>
+
+                        <label className={styles.timelabel}>
+                            What time do you take your first medication?
+                            <input
+                                className={styles.inputtime}
+                                type="time"
+                                value={time}
+                                placeholder="What time do you take your first medication?"
+                                onChange={(e) => setTime(e.target.value)}
+                            />
                         </label>
 
                         <Button className={styles.button}>
-                            <Link to="/information" className={styles.signlinks}>
+                            <Link
+                                to="/information"
+                                className={styles.signlinks}
+                            >
                                 Save
                             </Link>
                         </Button>
