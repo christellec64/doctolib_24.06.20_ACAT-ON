310b7ee736929e31c6bc9cc1a69819830b7a6f49
Christellec64 <christelle.cwcs64@gmail.com>
2020-06-26 10:03:55 +0200

    fix bug in  home page

--- doctolib/src/App.js
+++ doctolib/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import SignIn from "./components/login/Signin";
 import Home from "./components/Home";
 import Medication from "./components/Medication";
 import MainPage from "./components/login/wrapper";
@@ -17,7 +16,6 @@ function App() {
       <Router>
         <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/signin" component={SignIn} />
             <Route exact path="/createaccount" component={MainPage} />
             <Route exact path="/treatment" component={MyTreatment} />
             <Route exact path="/stat" component={Stat} />
